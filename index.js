9000866be8c662f87471e1298d4b6a7930a7589a
Dodoc Alexandru <alex.dodoc@yahoo.com>
2023-04-05 18:03:30 +0300

    move fetch into function

--- index.js
+++ index.js
@@ -38,15 +38,17 @@ function showSkills(skills) {
   var container = document.querySelector("#skills ul");
   container.innerHTML = html.join("");
 }
-
-fetch("skills.json")
-  .then(function (r) {
-    return r.json();
-    //  r.json().then(function (r2) {
-    //    console.warn("r2", r2);
-    //  });
-  })
-  .then(function (skills) {
-    // console.warn("r3", skills);
-    showSkills(skills);
-  });
+function loadSkills() {
+  fetch("skills.json")
+    .then(function (r) {
+      return r.json();
+      //  r.json().then(function (r2) {
+      //    console.warn("r2", r2);
+      //  });
+    })
+    .then(function (skills) {
+      // console.warn("r3", skills);
+      showSkills(skills);
+    });
+}
+loadSkills();
